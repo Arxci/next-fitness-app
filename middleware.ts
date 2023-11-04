423768d2fdfcc34a48deaaa59775cfb610e009c2
Arxci <garretthumbert9@gmail.com>
2023-11-04 18:42:33 -0500

    Comments added to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,16 @@
 import { authMiddleware, redirectToSignIn } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
 	publicRoutes: ['/', '/sign-in(.*)', '/sign-up(.*)', '/sso-callback(.*)'],
 
 	afterAuth(auth, req, evt) {
+		// Redirect un authorized users navigating to private routes to the sign in
 		if (!auth.userId && !auth.isPublicRoute) {
 			return redirectToSignIn({ returnBackUrl: req.url })
 		}
 
+		// Redirect users that have signed in already from the below auth pages
 		const authRoutes = ['/sign-in', '/sign-up', '/sign-up/verify-email']
 
 		if (auth.userId && authRoutes.includes(req.nextUrl.pathname)) {
