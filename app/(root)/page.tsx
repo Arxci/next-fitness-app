b86c612e25f09d5a81cdc61a44f181e8c508144b
Arxci <garretthumbert9@gmail.com>
2023-11-09 19:40:06 -0600

    CTA home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Separator } from '@/components/ui/separator'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
 	return (
@@ -26,14 +27,22 @@ function HeroSection() {
 						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 100vw"
 					/>
 					<div className="absolute w-full h-full bg-black/40" />
-					<div className="absolute container top-1/2 left-0 transform -translate-y-1/2 text-white space-y-2">
-						<h2 className="text-4xl font-bold">
-							Empower Your Journey with SkillJet
-						</h2>
-						<p className="text-white/80 text-lg max-w-[800px]">
-							Access a vast library of courses, expert insights, and tools
-							designed to drive your personal and professional development.
-						</p>
+					<div className="absolute container top-1/2 left-0 transform -translate-y-1/2 text-white space-y-4">
+						<div className="space-y-2">
+							<h2 className="text-4xl font-bold">
+								Empower Your Journey with SkillJet
+							</h2>
+							<p className="text-white/80 text-lg max-w-[800px]">
+								Access a vast library of courses, expert insights, and tools
+								designed to drive your personal and professional development.
+							</p>
+						</div>
+						<Button
+							size="lg"
+							className="w-full max-w-[200px]"
+						>
+							Register Now
+						</Button>
 					</div>
 				</div>
 			</div>
